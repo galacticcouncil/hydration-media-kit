5898aaedbd98e1439e7b431cd728477198981c8e
Jakub Pánik <jakub.panik@gmail.com>
2024-06-12 02:05:58 +0800

    update ascii, add h2o and hollar icons

--- ascii.rs
+++ ascii.rs
@@ -1,21 +1,27 @@
-//                  @@@@@@@@@@ 
-//                @@@@@@@@@@@@@@             
-//              @@@@@@@@@@@@@@@@@@           
-//            @@@@@@@@@@@@@@@@@@@@@@        
-//          @@         @@@@@@@@@@@@@@@                  _    _           _           _   _             
-//                          @@@@@@@@@@@                | |  | |         | |         | | (_)            
-//            @@@@@@@@@@@@@@@@@@@@@@@@@                | |__| |_   _  __| |_ __ __ _| |_ _  ___  _ __  
-//        @@@@@@@@@@@@@@@@@@@@@@@@@@@@                 |  __  | | | |/ _` | '__/ _` | __| |/ _ \| '_ \ 
-//      @@@@@@@@@@@@@@@@@@@@@@@@@@@@     @@@@          | |  | | |_| | (_| | | | (_| | |_| | (_) | | | |
-//    @@@@@@@@@@@@@@@@@@ @@@@         @@@@@@@@         |_|  |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
-//  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                  __/ |                                  
-//  @@@@@@@@         @@@@ @@@@@@@@@@@@@@@@@@                   |___/                                   
-//   @@@@     @@@@@@@@@@@@@@@@@@@@@@@@@@@@    
-//          @@@@@@@@@@@@@@@@@@@@@@@@@@@@    This file is part of https://github.com/galacticcouncil/Hydration-node
-//         @@@@@@@@@@@@@@@@@@@@@@@@@        Built with <3 for decentralisation.  
-//         @@@@@@@@@@@                      
-//          @@@@@@@@@@@@@@@         @@      Copyright (C) 2021-2024  Intergalactic, Limited (GIB).
-//            @@@@@@@@@@@@@@@@@@@@@@        SPDX-License-Identifier: Apache-2.0
-//              @@@@@@@@@@@@@@@@@@          Licensed under the Apache License, Version 2.0 (the "License");
-//                @@@@@@@@@@@@@@            you may not use this file except in compliance with the License.
-//                  @@@@@@@@@@              http://www.apache.org/licenses/LICENSE-2.0
+// This file is part of https://github.com/galacticcouncil/*
+//                              
+//                $$$$$$$      Licensed under the Apache License, Version 2.0 (the "License")
+//             $$$$$$$$$$$$$        you may only use this file in compliance with the License
+//          $$$$$$$$$$$$$$$$$$$            
+//                      $$$$$$$$$       Copyright (C) 2021-2024  Intergalactic, Limited (GIB)
+//         $$$$$$$$$$$   $$$$$$$$$$                       SPDX-License-Identifier: Apache-2.0
+//      $$$$$$$$$$$$$$$$$$$$$$$$$$                            
+//   $$$$$$$$$$$$$$$$$$$$$$$        $                      Built with <3 for decentralisation
+//  $$$$$$$$$$$$$$$$$$$        $$$$$$$                            
+//  $$$$$$$         $$$$$$$$$$$$$$$$$$      Unless required by applicable law or agreed to in 
+//   $       $$$$$$$$$$$$$$$$$$$$$$$       writing, software distributed under the License is 
+//      $$$$$$$$$$$$$$$$$$$$$$$$$$        distributed on an "AS IS" BASIS, WITHOUT WARRANTIES 
+//      $$$$$$$$$   $$$$$$$$$$$         OR CONDITIONS OF ANY KIND, either express or implied.
+//        $$$$$$$$                      
+//          $$$$$$$$$$$$$$$$$$            See the License for the specific language governing
+//             $$$$$$$$$$$$$                   permissions and limitations under the License.
+//                $$$$$$$             
+//                                                                 $$                        
+//  $$$$$   $$$$$                    $$                      $$                            
+//   $$$     $$$  $$$     $$   $$$$$ $$  $$$ $$$$  $$$$$$$  $$$$  $$$    $$$$$$   $$ $$$$$$  
+//   $$$     $$$   $$$   $$  $$$    $$$   $$$  $  $$     $$  $$    $$  $$     $$$  $$$   $$$ 
+//   $$$$$$$$$$$    $$  $$   $$$     $$   $$        $$$$$$$  $$    $$  $$     $$$  $$     $$ 
+//   $$$     $$$     $$$$    $$$     $$   $$     $$$     $$  $$    $$  $$$     $$  $$     $$ 
+//  $$$$$   $$$$$     $$      $$$   $$$   $$     $$$$   $$$  $$    $$   $$$   $$   $$    $$$ 
+//                   $$         $$$$$               $$$$$     $$          $$$$             
+//                 $$$                                                                                    
